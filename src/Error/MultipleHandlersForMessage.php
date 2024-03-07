95c1deb3140fe7cf90bc02dd5ae35871777fe644
Yonel Ceruto <yonel.ceruto@scnd.com>
2024-03-06 19:30:23 -0500

    Improvements

--- src/Error/MultipleHandlersForMessage.php
+++ src/Error/MultipleHandlersForMessage.php
@@ -4,8 +4,8 @@ namespace OpenSolid\Messenger\Error;
 
 final class MultipleHandlersForMessage extends \LogicException
 {
-    public static function create(string $class): self
+    public static function create(string $class, \Throwable $previous = null, int $code = 0): self
     {
-        return new self(sprintf('Message of type "%s" was handled multiple times. Only one handler is expected.', $class));
+        return new self(sprintf('Message of type "%s" was handled multiple times. Only one handler is expected.', $class), $code, $previous);
     }
 }
--- src/Error/NoHandlerForMessage.php
+++ src/Error/NoHandlerForMessage.php
@@ -4,8 +4,8 @@ namespace OpenSolid\Messenger\Error;
 
 final class NoHandlerForMessage extends \LogicException
 {
-    public static function create(string $class): self
+    public static function create(string $class, \Throwable $previous = null, int $code = 0): self
     {
-        return new self(sprintf('No handler for message "%s".', $class));
+        return new self(sprintf('No handler for message "%s".', $class), $code, $previous);
     }
 }
