331264f72468987b9047c01426de1894acc8b272
Yonel Ceruto <yonel.ceruto@scnd.com>
2024-04-16 20:22:34 -0400

    Add Doctrine Transaction Middleware

---
+++ src/Bridge/Doctrine/Middleware/DoctrineTransactionMiddleware.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace OpenSolid\Messenger\Bridge\Doctrine\Middleware;
+
+use Doctrine\ORM\EntityManagerInterface;
+use OpenSolid\Messenger\Middleware\Middleware;
+use OpenSolid\Messenger\Model\Envelope;
+
+readonly class DoctrineTransactionMiddleware implements Middleware
+{
+    public function __construct(private EntityManagerInterface $em)
+    {
+    }
+
+    public function handle(Envelope $envelope, callable $next): void
+    {
+        $this->em->wrapInTransaction(static fn () => $next($envelope));
+    }
+}
