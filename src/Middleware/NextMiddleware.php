adbc2dc53b7dd49a43cc0bc513844178e0fdc29f
Yonel Ceruto <yonel.ceruto@scnd.com>
2024-07-09 23:11:31 -0400

    remove internal mark for NextMiddleware

--- src/Middleware/NextMiddleware.php
+++ src/Middleware/NextMiddleware.php
@@ -4,9 +4,6 @@ namespace OpenSolid\Messenger\Middleware;
 
 use OpenSolid\Messenger\Model\Envelope;
 
-/**
- * @internal
- */
 interface NextMiddleware
 {
     public function handle(Envelope $envelope): void;
